a705cb239cc48e6a28e0459dae891425653ab564
paraglande03 <paraglande03@gmail.com>
2021-03-07 13:17:32 +0530

    Added : Method to find best rated Cheaper hotel

--- src/main/java/HotelRoomReservation.java
+++ src/main/java/HotelRoomReservation.java
@@ -1,58 +1,40 @@
 import java.util.*;
 public class HotelRoomReservation {
 
-    public HotelRoomReservation(){
-        setBridgeWood();
-        setLakeWood();
-        setRidgeWood();
-    }
 //    maps for hotels and prices
     private static HashMap<String, List<Integer>> lakeWood = new HashMap<>();
     private static HashMap<String, List<Integer>> bridgeWood = new HashMap<>();
     private static HashMap<String, List<Integer>> ridgeWood = new HashMap<>();
-
+    private static int lakeWoodRating;
+    private static int bridgeWoodRating;
+    private static int rigdeWoodRating;
+    int WeekDayLakeWoodCost= Integer.valueOf(setLakeWood().get(0));
+    int WeekDayBridgeWoodCost= Integer.valueOf(setBridgeWood().get(0));
+    int WeekDayRidgeWoodCost= Integer.valueOf(setRidgeWood().get(0));
+    int WeekendLakeWoodCost= Integer.valueOf(setLakeWood().get(1));
+    int WeekendBridgeWoodCost= Integer.valueOf(setBridgeWood().get(1));
+    int WeekendRidgeWoodCost= Integer.valueOf(setRidgeWood().get(1));
 
 
     public  static  List<Integer> setLakeWood() {
-
-
         List<Integer> lakeWoodRegular = Arrays.asList(110,90);
-
-
-
         lakeWood.put("Regular", lakeWoodRegular);
-
+        lakeWoodRating=3;
         return lakeWoodRegular;
     }
 
     public static List<Integer> setBridgeWood() {
-
-
         List<Integer> bridgeWoodRegular = Arrays.asList(160,60);
-
-
-
         bridgeWood.put("Regular", bridgeWoodRegular);
+        bridgeWoodRating=5;
         return bridgeWoodRegular;
     }
 
     public static  List<Integer> setRidgeWood() {
-
-
          List<Integer> ridgeWoodRegular = Arrays.asList(220,150);
-
-
-
-
         ridgeWood.put("Regular" , ridgeWoodRegular);
-
+        rigdeWoodRating=5;
             return ridgeWoodRegular;
-    }
-    public void printRates(){
-        System.out.println("Prices for regular customers weekdays / weekends resp.");
-        System.out.println("Lakewood Prices:- "+lakeWood.entrySet());
-        System.out.println("Bridgewood Prices:- "+bridgeWood.entrySet());
-        System.out.println("Ridgewood Prices:- "+ridgeWood.entrySet());
     }
 
 
@@ -63,39 +45,42 @@ public class HotelRoomReservation {
         System.out.println("Enter choice of weekdays of weekends:- 1.weekdays 2. weekends");
 
         int choice = scanner.nextInt();
+        int i =choice-1;
 
-        switch (choice) {
 
-            case 1:
-                if (Integer.valueOf(setLakeWood().get(0)) <= Integer.valueOf(setBridgeWood().get(0)) && Integer.valueOf(setLakeWood().get(0)) <= Integer.valueOf(setRidgeWood().get(0))) {
-                    System.out.println("lakewood is cheaper with price of : " + (Integer.valueOf(setLakeWood().get(0))) * noOfDays + "$");
+        if (Integer.valueOf(setLakeWood().get(i)) <= Integer.valueOf(setBridgeWood().get(i)) && Integer.valueOf(setLakeWood().get(i)) <= Integer.valueOf(setRidgeWood().get(i))) {
+            System.out.println("lakewood is cheaper with price of : " + (Integer.valueOf(setLakeWood().get(i))) * noOfDays + "$");
 
-                } else if (Integer.valueOf(setBridgeWood().get(0)) <= Integer.valueOf(setLakeWood().get(0)) && Integer.valueOf(setBridgeWood().get(0)) <= Integer.valueOf(setRidgeWood().get(0))) {
+        } else if (Integer.valueOf(setBridgeWood().get(i)) <= Integer.valueOf(setLakeWood().get(i)) && Integer.valueOf(setBridgeWood().get(i)) <= Integer.valueOf(setRidgeWood().get(i))) {
 
-                    System.out.println("Bridgewood is cheaper with price of : " + (Integer.valueOf(setBridgeWood().get(0))) * noOfDays + "$");
-                } else {
-                    System.out.println("Ridgewood is cheaper with price of : " + (Integer.valueOf(setRidgeWood().get(0))) * noOfDays + "$");
-
-                }
-
-                break;
-            case 2:
-                if (Integer.valueOf(setLakeWood().get(1)) <= Integer.valueOf(setBridgeWood().get(1)) && Integer.valueOf(setLakeWood().get(1)) <= Integer.valueOf(setRidgeWood().get(1))) {
-                    System.out.println("lakewood is cheaper with price of : " + (Integer.valueOf(setLakeWood().get(1))) * noOfDays + "$");
-
-                } else if (Integer.valueOf(setBridgeWood().get(1)) <= Integer.valueOf(setLakeWood().get(1)) && Integer.valueOf(setBridgeWood().get(1)) <= Integer.valueOf(setRidgeWood().get(1))) {
-
-                    System.out.println("Bridgewood is cheaper with price of : " + (Integer.valueOf(setBridgeWood().get(1))) * noOfDays + "$");
-                } else {
-                    System.out.println("Ridgewood is cheaper with price of : " + (Integer.valueOf(setRidgeWood().get(1))) * noOfDays + "$");
-
-                }
-
-                break;
-            default:
-                System.out.println("Enter valid choice");
+            System.out.println("Bridgewood is cheaper with price of : " + (Integer.valueOf(setBridgeWood().get(i))) * noOfDays + "$");
+        } else {
+            System.out.println("Ridgewood is cheaper with price of : " + (Integer.valueOf(setRidgeWood().get(i))) * noOfDays + "$");
 
         }
     }
+    public static String minCostBestRated(int lakeWoodCost, int bridgeWoodCost, int ridgeWoodCost) {
+        int minCost = Math.min(lakeWoodCost, Math.min(bridgeWoodCost, ridgeWoodCost));
+        if (minCost == lakeWoodCost && minCost == bridgeWoodCost) {
+            return bridgeWoodRating > lakeWoodRating ? "BridgeWood" : "LakeWood";
+        } else if (minCost == bridgeWoodCost && minCost == ridgeWoodCost) {
+            return bridgeWoodRating > ridgeWoodCost ? "BridgeWood" : "RidgeWood";
+        } else if (minCost == lakeWoodCost && minCost == ridgeWoodCost) {
+            return lakeWoodCost > ridgeWoodCost ? "LakeWood" : "RidgeWood";
+        } else {
+            if (minCost == lakeWoodCost) {
+
+                System.out.println("Lakewood is best rated cheaper hotel");
+                return "lakewood";
+            } else if (minCost == bridgeWoodCost) {
+                System.out.println("Bridgewood is best rated cheaper hotel");
+                return "Bridgewood";
+            } else {
+                System.out.println("Ridgewood is best rated cheaper hotel");
+                return "RidgeWood";
+            }
+        }
+    }
+
 }
 
--- src/main/java/MainReservation.java
+++ src/main/java/MainReservation.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class MainReservation {
     public static void main(String[] args) {
-        HotelRoomReservation hotelRoomReservation =new HotelRoomReservation();
-        hotelRoomReservation.printRates();
-        HotelRoomReservation.getTotalRate();
+        HotelRoomReservation hotelRoomReservation=new HotelRoomReservation();
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter choice for \n1.total rate for weekend &weekdays\n2. rates for best rated weekday prices\n3. rates for best rated weekend prices ");
+        int byCategory= scanner.nextInt();
+        switch (byCategory){
+            case 1:
+                HotelRoomReservation.getTotalRate();
+                break;
+            case 2:
+                HotelRoomReservation.minCostBestRated(hotelRoomReservation.WeekDayLakeWoodCost, hotelRoomReservation.WeekDayBridgeWoodCost, hotelRoomReservation.WeekDayRidgeWoodCost);
+                break;
+            case 3:
+                HotelRoomReservation.minCostBestRated(hotelRoomReservation.WeekendLakeWoodCost, hotelRoomReservation.WeekendBridgeWoodCost, hotelRoomReservation.WeekendRidgeWoodCost);
+                break;
+            default:
+                System.out.println("Enter valid option");
+        }
+
+
+
     }
 }
+//
+//
